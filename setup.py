eb9e003177d10790b7316fd5ebef6d3b898b3cab
Malte S. Kurz <malte.s.kurz@gmail.com>
2020-11-17 15:41:00 +0100

    added initial requirement file and setup.py for dml serverless package

---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+#PROJECT_URLS = {
+#    'Bug Tracker': 'https://github.com/DoubleML/doubleml-serverless/issues',
+#    'Source Code': 'https://github.com/DoubleML/doubleml-serverless'
+#}
+
+setup(
+    name='DoubleML-Serverless',
+    version='0.1.dev0',
+    author='Kurz, M. S.',
+    maintainer='Malte S. Kurz',
+    maintainer_email='malte.simon.kurz@uni-hamburg.de',
+    description='Double Machine Learning with Serverless Scaling',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='http://doubleml.org',
+    packages=find_packages(),
+    install_requires=[
+        'joblib',
+        'numpy',
+        'pandas',
+        'scipy',
+        'sklearn',
+        'statsmodels',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
