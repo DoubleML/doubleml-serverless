ebf5c4bad9a55139386e606d8e25845e98f521af
Malte S. Kurz <malte.s.kurz@gmail.com>
2020-11-23 09:35:43 +0100

    added iivm implementation

---
+++ doubleml_serverless/double_ml_iivm_aws_lambda.py
@@ -0,0 +1,140 @@
+from doubleml import DoubleMLIIVM
+import numpy as np
+from sklearn.utils import check_X_y
+
+from ._helper import _get_cond_smpls
+
+from .double_ml_aws_lambda import DoubleMLLambda
+from ._helper import _attach_learner, _attach_smpls, _extract_preds
+
+
+class DoubleMLIIVMServerless(DoubleMLIIVM, DoubleMLLambda):
+    def __init__(self,
+                 lambda_function_name,
+                 aws_region,
+                 obj_dml_data,
+                 ml_g,
+                 ml_m,
+                 ml_r,
+                 n_folds=5,
+                 n_rep=1,
+                 score='ATE',
+                 dml_procedure='dml2',
+                 trimming_rule='truncate',
+                 trimming_threshold=1e-12,
+                 draw_sample_splitting=True,
+                 apply_cross_fitting=True):
+        DoubleMLIIVM.__init__(self,
+                              obj_dml_data,
+                              ml_g,
+                              ml_m,
+                              ml_r,
+                              n_folds,
+                              n_rep,
+                              score,
+                              dml_procedure,
+                              trimming_rule,
+                              trimming_threshold,
+                              draw_sample_splitting,
+                              apply_cross_fitting)
+        DoubleMLLambda.__init__(self,
+                                lambda_function_name,
+                                aws_region)
+
+    # this method overwrites DoubleML.fit() to implement the fit via aws lambda
+    def fit(self, n_jobs_cv='n_folds * n_rep', keep_scores=True):
+        """
+        Parameters
+        ----------
+        n_jobs_cv : str
+
+        keep_scores : bool
+        """
+
+        if (not isinstance(n_jobs_cv, str)) | (n_jobs_cv not in ['n_folds * n_rep', 'n_rep']):
+            raise ValueError('n_jobs_cv must be "n_folds * n_rep" or "n_rep"'
+                             f' got {str(n_jobs_cv)}')
+
+        assert self._dml_data.n_treat == 1
+        self._i_treat = 0
+
+        # ml estimation of nuisance models and computation of score elements
+        psi_a, psi_b = self._ml_nuisance_and_score_elements(self.smpls, n_jobs_cv)
+        self._psi_a[:, :, self._i_treat] = psi_a
+        self._psi_b[:, :, self._i_treat] = psi_b
+
+        self._est_causal_pars_and_se()
+
+        if not keep_scores:
+            self._clean_scores()
+
+        return self
+
+    def _ml_nuisance_and_score_elements(self, smpls, n_jobs_cv):
+        x, y = check_X_y(self._dml_data.x, self._dml_data.y)
+        x, z = check_X_y(x, np.ravel(self._dml_data.z))
+        x, d = check_X_y(x, self._dml_data.d)
+        # get train indices for z == 0 and z == 1
+        smpls_z0, smpls_z1 = _get_cond_smpls(smpls, z)
+
+        payload = self._dml_data.get_payload()
+
+        payload_ml_g0 = payload.copy()
+        payload_ml_g1 = payload.copy()
+        payload_ml_m = payload.copy()
+        payload_ml_r0 = payload.copy()
+        payload_ml_r1 = payload.copy()
+
+        _attach_learner(payload_ml_g0,
+                        'ml_g0', self.learner['ml_g'],
+                        self._dml_data.y_col, self._dml_data.x_cols)
+
+        _attach_learner(payload_ml_g1,
+                        'ml_g1', self.learner['ml_g'],
+                        self._dml_data.y_col, self._dml_data.x_cols)
+
+        _attach_learner(payload_ml_m,
+                        'ml_m', self.learner['ml_m'],
+                        self._dml_data.z_cols[0], self._dml_data.x_cols,
+                        method='predict_proba')
+
+        _attach_learner(payload_ml_r0,
+                        'ml_r0', self.learner['ml_r'],
+                        self._dml_data.d_cols[0], self._dml_data.x_cols,
+                        method='predict_proba')
+
+        _attach_learner(payload_ml_r1,
+                        'ml_r1', self.learner['ml_r'],
+                        self._dml_data.d_cols[0], self._dml_data.x_cols,
+                        method='predict_proba')
+
+        all_payloads = [payload_ml_g0, payload_ml_g1, payload_ml_m, payload_ml_r0, payload_ml_r1]
+        all_smpls = [smpls_z0, smpls_z1, smpls, smpls_z0, smpls_z1]
+
+        payloads = _attach_smpls(all_payloads,
+                                 all_smpls,
+                                 self.n_rep,
+                                 self._dml_data.n_obs,
+                                 n_jobs_cv,
+                                 [True, True, False, True, True])
+
+        results = self.invoke_lambdas(payloads)
+
+        preds = _extract_preds(results, smpls, self.params_names,
+                               self._dml_data.n_obs, self.n_rep,
+                               n_jobs_cv)
+
+        psi_a = np.full((self._dml_data.n_obs, self.n_rep), np.nan)
+        psi_b = np.full((self._dml_data.n_obs, self.n_rep), np.nan)
+
+        for i_rep in range(self.n_rep):
+            # compute score elements
+            psi_a[:, i_rep], psi_b[:, i_rep] = self._score_elements(y, z, d,
+                                                                    preds['ml_g0'][:, i_rep],
+                                                                    preds['ml_g1'][:, i_rep],
+                                                                    preds['ml_m'][:, i_rep],
+                                                                    preds['ml_r0'][:, i_rep],
+                                                                    preds['ml_r1'][:, i_rep],
+                                                                    smpls[i_rep])
+
+        return psi_a, psi_b
---
+++ doubleml_serverless/tests/test_iivm.py
@@ -0,0 +1,216 @@
+import numpy as np
+import pandas as pd
+import pytest
+import math
+
+from sklearn.base import clone
+
+from sklearn.linear_model import LogisticRegression, LinearRegression
+from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
+
+import doubleml as dml
+import doubleml_serverless as dml_lambda
+
+from doubleml_serverless.tests.helper_general import get_n_datasets
+
+# number of datasets per dgp
+n_datasets = get_n_datasets()
+
+
+@pytest.fixture(scope='module',
+                params=range(n_datasets))
+def idx(request):
+    return request.param
+
+
+@pytest.fixture(scope='module',
+                params=[[LogisticRegression(solver='lbfgs', max_iter=250),
+                         LinearRegression()],
+                        [RandomForestClassifier(max_depth=2, n_estimators=10),
+                         RandomForestRegressor(max_depth=2, n_estimators=10)]])
+def learner(request):
+    return request.param
+
+
+@pytest.fixture(scope='module',
+                params=['LATE'])
+def score(request):
+    return request.param
+
+
+@pytest.fixture(scope='module',
+                params=['dml1', 'dml2'])
+def dml_procedure(request):
+    return request.param
+
+
+@pytest.fixture(scope='module',
+                params=[0.01, 0.05])
+def trimming_threshold(request):
+    return request.param
+
+
+@pytest.fixture(scope="module")
+def dml_iivm_fixture(generate_data_iivm, idx, learner, score, dml_procedure, trimming_threshold):
+    boot_methods = ['normal']
+    n_folds = 4
+    n_rep_boot = 502
+
+    # collect data
+    data = generate_data_iivm[idx]
+
+    # to simulate lambda calls we have dumps to json, so we simulate it here to prevent differences due to inaccuracies
+    data = pd.read_json(data.to_json(orient='columns'), orient='columns')
+
+    x_cols = data.columns[data.columns.str.startswith('X')].tolist()
+
+    # Set machine learning methods for m & g
+    ml_g = clone(learner[1])
+    ml_m = clone(learner[0])
+    ml_r = clone(learner[0])
+
+    np.random.seed(3141)
+    dml_data_json = dml_lambda.DoubleMLDataJson(data, 'y', ['d'], x_cols, 'z')
+    dml_iivm_lambda = dml_lambda.DoubleMLIIVMServerless('local', 'local',
+                                                        dml_data_json,
+                                                        ml_g, ml_m, ml_r,
+                                                        n_folds,
+                                                        score=score,
+                                                        dml_procedure=dml_procedure)
+
+    dml_iivm_lambda.fit()
+
+    np.random.seed(3141)
+    dml_data = dml.DoubleMLData(data, 'y', ['d'], x_cols, 'z')
+    dml_iivm = dml.DoubleMLIIVM(dml_data,
+                                ml_g, ml_m, ml_r,
+                                n_folds,
+                                score=score,
+                                dml_procedure=dml_procedure)
+
+    dml_iivm.fit()
+
+    res_dict = {'coef': dml_iivm.coef,
+                'coef_lambda': dml_iivm_lambda.coef,
+                'se': dml_iivm.se,
+                'se_lambda': dml_iivm_lambda.se,
+                'boot_methods': boot_methods}
+
+    for bootstrap in boot_methods:
+        np.random.seed(3141)
+        dml_iivm.bootstrap(method=bootstrap, n_rep_boot=n_rep_boot)
+        np.random.seed(3141)
+        dml_iivm_lambda.bootstrap(method=bootstrap, n_rep_boot=n_rep_boot)
+
+        res_dict['boot_coef' + bootstrap] = dml_iivm.boot_coef
+        res_dict['boot_t_stat' + bootstrap] = dml_iivm.boot_t_stat
+        res_dict['boot_coef' + bootstrap + '_lambda'] = dml_iivm_lambda.boot_coef
+        res_dict['boot_t_stat' + bootstrap + '_lambda'] = dml_iivm_lambda.boot_t_stat
+
+    return res_dict
+
+
+def test_dml_iivm_coef(dml_iivm_fixture):
+    assert math.isclose(dml_iivm_fixture['coef'],
+                        dml_iivm_fixture['coef_lambda'],
+                        rel_tol=1e-9, abs_tol=1e-4)
+
+
+def test_dml_iivm_se(dml_iivm_fixture):
+    assert math.isclose(dml_iivm_fixture['se'],
+                        dml_iivm_fixture['se_lambda'],
+                        rel_tol=1e-9, abs_tol=1e-4)
+
+
+def test_dml_iivm_boot(dml_iivm_fixture):
+    for bootstrap in dml_iivm_fixture['boot_methods']:
+        assert np.allclose(dml_iivm_fixture['boot_coef' + bootstrap],
+                           dml_iivm_fixture['boot_coef' + bootstrap + '_lambda'],
+                           rtol=1e-9, atol=1e-4)
+        assert np.allclose(dml_iivm_fixture['boot_t_stat' + bootstrap],
+                           dml_iivm_fixture['boot_t_stat' + bootstrap + '_lambda'],
+                           rtol=1e-9, atol=1e-4)
+
+
+@pytest.fixture(scope="module")
+def dml_iivm_scaling_fixture(generate_data_iivm, idx, learner, score, dml_procedure):
+    boot_methods = ['normal']
+    n_folds = 4
+    n_rep_boot = 502
+
+    # collect data
+    data = generate_data_iivm[idx]
+
+    # to simulate lambda calls we have dumps to json, so we simulate it here to prevent differences due to inaccuracies
+    data = pd.read_json(data.to_json(orient='columns'), orient='columns')
+
+    x_cols = data.columns[data.columns.str.startswith('X')].tolist()
+
+    # Set machine learning methods for m & g
+    ml_g = clone(learner[1])
+    ml_m = clone(learner[0])
+    ml_r = clone(learner[0])
+
+    dml_data_json = dml_lambda.DoubleMLDataJson(data, 'y', ['d'], x_cols, 'z')
+
+    np.random.seed(3141)
+    dml_iivm_folds = dml_lambda.DoubleMLIIVMServerless('local', 'local',
+                                                       dml_data_json,
+                                                       ml_g, ml_m, ml_r,
+                                                       n_folds,
+                                                       score=score,
+                                                       dml_procedure=dml_procedure)
+
+    dml_iivm_folds.fit('n_folds * n_rep')
+
+    np.random.seed(3141)
+    dml_iivm_reps = dml_lambda.DoubleMLIIVMServerless('local', 'local',
+                                                      dml_data_json,
+                                                      ml_g, ml_m, ml_r,
+                                                      n_folds,
+                                                      score=score,
+                                                      dml_procedure=dml_procedure)
+
+    dml_iivm_reps.fit('n_rep')
+
+    res_dict = {'coef_folds': dml_iivm_folds.coef,
+                'coef_reps': dml_iivm_reps.coef,
+                'se_folds': dml_iivm_folds.se,
+                'se_reps': dml_iivm_reps.se,
+                'boot_methods': boot_methods}
+
+    for bootstrap in boot_methods:
+        np.random.seed(3141)
+        dml_iivm_folds.bootstrap(method=bootstrap, n_rep_boot=n_rep_boot)
+        np.random.seed(3141)
+        dml_iivm_reps.bootstrap(method=bootstrap, n_rep_boot=n_rep_boot)
+
+        res_dict['boot_coef' + bootstrap + '_folds'] = dml_iivm_folds.boot_coef
+        res_dict['boot_t_stat' + bootstrap + '_folds'] = dml_iivm_folds.boot_t_stat
+        res_dict['boot_coef' + bootstrap + '_reps'] = dml_iivm_reps.boot_coef
+        res_dict['boot_t_stat' + bootstrap + '_reps'] = dml_iivm_reps.boot_t_stat
+
+    return res_dict
+
+
+def test_dml_iivm_scaling_coef(dml_iivm_scaling_fixture):
+    assert math.isclose(dml_iivm_scaling_fixture['coef_folds'],
+                        dml_iivm_scaling_fixture['coef_reps'],
+                        rel_tol=1e-9, abs_tol=1e-4)
+
+
+def test_dml_iivm_scaling_se(dml_iivm_scaling_fixture):
+    assert math.isclose(dml_iivm_scaling_fixture['se_folds'],
+                        dml_iivm_scaling_fixture['se_reps'],
+                        rel_tol=1e-9, abs_tol=1e-4)
+
+
+def test_dml_iivm_scaling_boot(dml_iivm_scaling_fixture):
+    for bootstrap in dml_iivm_scaling_fixture['boot_methods']:
+        assert np.allclose(dml_iivm_scaling_fixture['boot_coef' + bootstrap + '_folds'],
+                           dml_iivm_scaling_fixture['boot_coef' + bootstrap + '_reps'],
+                           rtol=1e-9, atol=1e-4)
+        assert np.allclose(dml_iivm_scaling_fixture['boot_t_stat' + bootstrap + '_folds'],
+                           dml_iivm_scaling_fixture['boot_t_stat' + bootstrap + '_reps'],
+                           rtol=1e-9, atol=1e-4)
+
