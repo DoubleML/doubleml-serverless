018da21038341323a0e5fd6e56ad5d604669d3c4
Malte S. Kurz <malte.s.kurz@gmail.com>
2020-12-17 11:54:24 +0100

    move files

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='http://doubleml.org',
-    packages=find_packages(),
+    packages=find_packages(exclude=['aws_lambda_app*']),
     install_requires=[
         'joblib',
         'numpy',
